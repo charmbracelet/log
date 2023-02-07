28b265478731f2d00abf618cab2db7f6ff3bdd99
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-02-07 18:31:33 -0500

    chore: add fatal & multiline tests

--- log_test.go
+++ log_test.go
@@ -3,6 +3,8 @@ package log
 import (
 	"bytes"
 	"fmt"
+	"os"
+	"os/exec"
 	"path/filepath"
 	"runtime"
 	"testing"
@@ -55,6 +57,13 @@ func TestLogger(t *testing.T) {
 			kvs:      []interface{}{"key1", "val1", "key2", "val2"},
 			f:        logger.Error,
 		},
+		{
+			name:     "error message with multiline",
+			expected: "ERROR info\n  key1=\n  │ val1\n  │ val2\n",
+			msg:      "info",
+			kvs:      []interface{}{"key1", "val1\nval2"},
+			f:        logger.Error,
+		},
 	}
 	for _, c := range cases {
 		buf.Reset()
@@ -65,7 +74,7 @@ func TestLogger(t *testing.T) {
 	}
 }
 
-func TestOffLevel(t *testing.T) {
+func TestLogOffLevel(t *testing.T) {
 	var buf bytes.Buffer
 	logger := New(WithOutput(&buf), WithTimeFunction(_zeroTime),
 		WithNoStyles(), WithLevel(OffLevel))
@@ -107,4 +116,21 @@ func TestLogHelper(t *testing.T) {
 	_, file, line, ok := runtime.Caller(0)
 	require.True(t, ok)
 	assert.Equal(t, fmt.Sprintf("INFO <log/%s:%d> helper func\n", filepath.Base(file), line-1), buf.String())
+}
+
+func TestLogFatal(t *testing.T) {
+	var buf bytes.Buffer
+	logger := New(WithOutput(&buf),
+		WithCaller(), WithNoStyles())
+	if os.Getenv("FATAL") == "1" {
+		logger.Fatal("i'm dead")
+		return
+	}
+	cmd := exec.Command(os.Args[0], "-test.run=TestLogFatal")
+	cmd.Env = append(os.Environ(), "FATAL=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return
+	}
+	t.Fatalf("process ran with err %v, want exit status 1", err)
 }
--- pkg_test.go
+++ pkg_test.go
@@ -2,6 +2,8 @@ package log
 
 import (
 	"bytes"
+	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,11 +22,25 @@ func TestGlobal(t *testing.T) {
 		f        func(msg interface{}, kvs ...interface{})
 	}{
 		{
-			name:     "default logger with timestamp",
+			name:     "default logger info with timestamp",
 			expected: "0001/01/01 00:00:00 INFO info\n",
 			msg:      "info",
 			kvs:      nil,
 			f:        Info,
+		},
+		{
+			name:     "default logger debug with timestamp",
+			expected: "",
+			msg:      "info",
+			kvs:      nil,
+			f:        Debug,
+		},
+		{
+			name:     "default logger error with timestamp",
+			expected: "0001/01/01 00:00:00 ERROR info\n",
+			msg:      "info",
+			kvs:      nil,
+			f:        Error,
 		},
 	}
 	for _, c := range cases {
@@ -34,4 +50,29 @@ func TestGlobal(t *testing.T) {
 			assert.Equal(t, c.expected, buf.String())
 		})
 	}
+}
+
+func TestPrint(t *testing.T) {
+	var buf bytes.Buffer
+	SetOutput(&buf)
+	SetLevel(FatalLevel)
+	SetTimeFunction(_zeroTime)
+	DisableStyles()
+	Error("error")
+	Print("print")
+	assert.Equal(t, "0001/01/01 00:00:00 print\n", buf.String())
+}
+
+func TestFatal(t *testing.T) {
+	if os.Getenv("FATAL") == "1" {
+		Fatal("i'm dead")
+		return
+	}
+	cmd := exec.Command(os.Args[0], "-test.run=TestFatal")
+	cmd.Env = append(os.Environ(), "FATAL=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return
+	}
+	t.Fatalf("process ran with err %v, want exit status 1", err)
 }
