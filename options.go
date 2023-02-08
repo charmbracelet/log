a49372502c2ba3e0efa8fe38e69d73093d84a906
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-02-08 10:28:30 -0500

    docs: update godocs

--- options.go
+++ options.go
@@ -2,28 +2,32 @@ package log
 
 import "io"
 
-// WithOutput returns a LoggerOption that sets the output for the logger.
+// WithOutput returns a LoggerOption that sets the output for the logger. The
+// default is os.Stderr.
 func WithOutput(w io.Writer) LoggerOption {
 	return func(l *logger) {
 		l.w = w
 	}
 }
 
-// WithTimeFunction returns a LoggerOption that sets the time function for the logger.
+// WithTimeFunction returns a LoggerOption that sets the time function for the
+// logger. The default is time.Now.
 func WithTimeFunction(f TimeFunction) LoggerOption {
 	return func(l *logger) {
 		l.timeFunc = f
 	}
 }
 
-// WithTimeFormat returns a LoggerOption that sets the time format for the logger.
+// WithTimeFormat returns a LoggerOption that sets the time format for the
+// logger. The default is "2006/01/02 15:04:05".
 func WithTimeFormat(format string) LoggerOption {
 	return func(l *logger) {
 		l.timeFormat = format
 	}
 }
 
-// WithLevel returns a LoggerOption that sets the level for the logger.
+// WithLevel returns a LoggerOption that sets the level for the logger. The
+// default is InfoLevel.
 func WithLevel(level Level) LoggerOption {
 	return func(l *logger) {
 		l.level = level
