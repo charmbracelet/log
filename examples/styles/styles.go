a9f75d2465861878b67f0216fd49c75b54d69132
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-02-22 15:28:22 -0500

    docs: add styles section and examples

---
+++ examples/styles/styles.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"time"
+
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/log"
+)
+
+func main() {
+	log.ErrorLevelStyle = lipgloss.NewStyle().
+		SetString("ERROR!!").
+		Padding(0, 1, 0, 1).
+		Background(lipgloss.Color("204")).
+		Foreground(lipgloss.Color("0"))
+	log.KeyStyles["err"] = lipgloss.NewStyle().Foreground(lipgloss.Color("204"))
+	logger := log.New()
+	logger.Error("Whoops!", "err", "kitchen on fire")
+	time.Sleep(3 * time.Second)
+}
