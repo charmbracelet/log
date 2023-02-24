baccac60cd80c2faa3ef081e33482fcadd687580
Gökhan Özeloğlu <33967642+gozeloglu@users.noreply.github.com>
2023-02-24 19:07:03 +0300

    feat: parse level from string (#11)
    * Read log level from env var
    
    * Fixes #9.
    
    * With these changes, the log level will be able to read from the env var with
    `LOG_LEVEL` key.
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * Replace os.Getenv with t.Getenv
    
    * Update .gitignore.
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * Add `WithLevelFromString` option
    
    * `readLevelFromEnv` function is removed.
    
    * Tests updated.
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * Add `ParseLevel` function
    
    * This function converts string to Level type.
    
    * Unit test implemented.
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * Remove switch-case blocks
    
    * Replace swich-case blocks with `ParseLevel` function in `WithLevelFromString`.
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * Remove `WithLevelFromString` option
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * Update tests
    
    * Revert changes in `log_test.go`.
    
    * Update test cases in `level_test.go`.
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * Remove empty line
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * Remove env var related test cases
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * Remove unneeded line
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    ---------
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>

--- level_test.go
+++ level_test.go
@@ -10,3 +10,52 @@ func TestDefaultLevel(t *testing.T) {
 	var level Level
 	assert.Equal(t, InfoLevel, level)
 }
+func TestParseLevel(t *testing.T) {
+	testCases := []struct {
+		name     string
+		level    string
+		expLevel Level
+	}{
+		{
+			name:     "Parse debug",
+			level:    "debug",
+			expLevel: DebugLevel,
+		},
+		{
+			name:     "Parse info",
+			level:    "Info",
+			expLevel: InfoLevel,
+		},
+		{
+			name:     "Parse warn",
+			level:    "WARN",
+			expLevel: WarnLevel,
+		},
+		{
+			name:     "Parse error",
+			level:    "error",
+			expLevel: ErrorLevel,
+		},
+		{
+			name:     "Parse fatal",
+			level:    "FATAL",
+			expLevel: FatalLevel,
+		},
+		{
+			name:     "Default",
+			level:    "",
+			expLevel: InfoLevel,
+		},
+		{
+			name:     "Wrong level, set INFO",
+			level:    "WRONG_LEVEL",
+			expLevel: InfoLevel,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.expLevel, ParseLevel(tc.level))
+		})
+	}
+}
