33321ea65eb54efc6bad5ed4e1aa86d0dfa2f3b7
Drewry Pope <drewrypope@gmail.com>
2023-10-30 04:16:37 -0500

    add test

--- logger_test.go
+++ logger_test.go
@@ -129,6 +129,47 @@ func TestLogFormatter(t *testing.T) {
 	}
 }
 
+func TestEmptyMessage(t *testing.T) {
+	var buf bytes.Buffer
+	l := New(&buf)
+	cases := []struct {
+		name     string
+		expected string
+		msg      string
+		fields   []interface{}
+		kvs      []interface{}
+	}{
+		{
+			name:     "empty message nil fields",
+			expected: "INFO\n",
+			msg:      "",
+			fields:   nil,
+			kvs:      nil,
+		},
+		{
+			name:     "empty message with fields",
+			expected: "INFO foo=bar\n",
+			msg:      "",
+			fields:   []interface{}{"foo", "bar"},
+			kvs:      nil,
+		},
+		{
+			name:     "empty message with fields & kvs",
+			expected: "INFO foo=bar foobar=baz\n",
+			msg:      "",
+			fields:   []interface{}{"foo", "bar"},
+			kvs:      []interface{}{"foobar", "baz"},
+		},
+	}
+	for _, c := range cases {
+		buf.Reset()
+		t.Run(c.name, func(t *testing.T) {
+			l.With(c.fields...).Info(c.msg, c.kvs...)
+			assert.Equal(t, c.expected, buf.String())
+		})
+	}
+}
+
 func TestLogWithPrefix(t *testing.T) {
 	var buf bytes.Buffer
 	cases := []struct {
