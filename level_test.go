da2bdc850a1a54598c07bb407424208734b32045
Drewry Pope <Drewry.Pope@vertexinc.com>
2023-10-31 21:49:12 -0500

    expected

--- level_test.go
+++ level_test.go
@@ -14,59 +14,59 @@ func TestDefaultLevel(t *testing.T) {
 
 func TestParseLevel(t *testing.T) {
 	testCases := []struct {
-		name   string
-		input  string
-		result Level
-		error  error
+		name     string
+		input    string
+		expected Level
+		error    error
 	}{
 		{
-			name:   "Parse debug",
-			input:  "debug",
-			result: DebugLevel,
-			error:  nil,
+			name:     "Parse debug",
+			input:    "debug",
+			expected: DebugLevel,
+			error:    nil,
 		},
 		{
-			name:   "Parse info",
-			input:  "Info",
-			result: InfoLevel,
-			error:  nil,
+			name:     "Parse info",
+			input:    "Info",
+			expected: InfoLevel,
+			error:    nil,
 		},
 		{
-			name:   "Parse warn",
-			input:  "WARN",
-			result: WarnLevel,
-			error:  nil,
+			name:     "Parse warn",
+			input:    "WARN",
+			expected: WarnLevel,
+			error:    nil,
 		},
 		{
-			name:   "Parse error",
-			input:  "error",
-			result: ErrorLevel,
-			error:  nil,
+			name:     "Parse error",
+			input:    "error",
+			expected: ErrorLevel,
+			error:    nil,
 		},
 		{
-			name:   "Parse fatal",
-			input:  "FATAL",
-			result: FatalLevel,
-			error:  nil,
+			name:     "Parse fatal",
+			input:    "FATAL",
+			expected: FatalLevel,
+			error:    nil,
 		},
 		{
-			name:   "Default",
-			input:  "",
-			result: InfoLevel,
-			error:  fmt.Errorf("%w: %q", ErrInvalidLevel, ""),
+			name:     "Default",
+			input:    "",
+			expected: InfoLevel,
+			error:    fmt.Errorf("%w: %q", ErrInvalidLevel, ""),
 		},
 		{
-			name:   "Wrong level, set INFO",
-			input:  "WRONG_LEVEL",
-			result: InfoLevel,
-			error:  fmt.Errorf("%w: %q", ErrInvalidLevel, "WRONG_LEVEL"),
+			name:     "Wrong level, set INFO",
+			input:    "WRONG_LEVEL",
+			expected: InfoLevel,
+			error:    fmt.Errorf("%w: %q", ErrInvalidLevel, "WRONG_LEVEL"),
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			lvl, err := ParseLevel(tc.input)
-			assert.Equal(t, tc.result, lvl)
+			assert.Equal(t, tc.expected, lvl)
 			assert.Equal(t, tc.error, err)
 		})
 	}
