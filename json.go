dd7bc9494cf4ad37c25220dba6703ac257f513c1
Örjan Fors <o@42mm.org>
2024-05-03 11:48:39 +0200

    feat: preserve order when writing json

--- json.go
+++ json.go
@@ -1,34 +1,36 @@
 package log
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"time"
 )
 
 func (l *Logger) jsonFormatter(keyvals ...interface{}) {
-	m := make(map[string]interface{}, len(keyvals)/2)
+	jw := jsonWriter{w: &l.b}
+	jw.start()
 	for i := 0; i < len(keyvals); i += 2 {
 		switch keyvals[i] {
 		case TimestampKey:
 			if t, ok := keyvals[i+1].(time.Time); ok {
-				m[TimestampKey] = t.Format(l.timeFormat)
+				jw.write(TimestampKey, t.Format(l.timeFormat))
 			}
 		case LevelKey:
 			if level, ok := keyvals[i+1].(Level); ok {
-				m[LevelKey] = level.String()
+				jw.write(LevelKey, level.String())
 			}
 		case CallerKey:
 			if caller, ok := keyvals[i+1].(string); ok {
-				m[CallerKey] = caller
+				jw.write(CallerKey, caller)
 			}
 		case PrefixKey:
 			if prefix, ok := keyvals[i+1].(string); ok {
-				m[PrefixKey] = prefix
+				jw.write(PrefixKey, prefix)
 			}
 		case MessageKey:
 			if msg := keyvals[i+1]; msg != nil {
-				m[MessageKey] = fmt.Sprint(msg)
+				jw.write(MessageKey, fmt.Sprint(msg))
 			}
 		default:
 			var (
@@ -51,11 +53,60 @@ func (l *Logger) jsonFormatter(keyvals ...interface{}) {
 			default:
 				val = v
 			}
-			m[key] = val
+			jw.write(key, val)
 		}
 	}
+	jw.end()
+}
 
-	e := json.NewEncoder(&l.b)
+type jsonWriter struct {
+	w *bytes.Buffer
+}
+
+func (w *jsonWriter) start() {
+	w.w.WriteRune('{')
+}
+
+func (w *jsonWriter) end() {
+	w.w.WriteRune('}')
+	w.w.WriteRune('\n')
+}
+
+func (w *jsonWriter) write(key string, value any) {
+	// store pos if we need to rewind
+	pos := w.w.Len()
+
+	// add separator when buffer is longer than '{'
+	if w.w.Len() > 1 {
+		w.w.WriteRune(',')
+	}
+
+	err := w.writeEncoded(key)
+	if err != nil {
+		w.w.Truncate(pos)
+		return
+	}
+	w.w.WriteRune(':')
+
+	pos = w.w.Len()
+	err = w.writeEncoded(value)
+	if err != nil {
+		w.w.Truncate(pos)
+		w.w.WriteString(`"invalid value"`)
+	}
+}
+
+func (w *jsonWriter) writeEncoded(v any) error {
+	e := json.NewEncoder(w.w)
 	e.SetEscapeHTML(false)
-	_ = e.Encode(m)
+	if err := e.Encode(v); err != nil {
+		return err
+	}
+
+	// trailing \n added by json.Encode
+	b := w.w.Bytes()
+	if len(b) > 0 && b[len(b)-1] == '\n' {
+		w.w.Truncate(w.w.Len() - 1)
+	}
+	return nil
 }
